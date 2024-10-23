6d4e12ce6e1e0925f1d575227239a1452b37cb74
smizal <sjs29@hotmail.com>
2024-10-23 10:57:50 +0300

    EJS Lab

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+const RESTAURANT = {
+  name: 'The Green Byte Bistro',
+  isOpen: true,
+  address: '742 Evergreen Rd, Mapleview, OS 45502',
+  phone: '555-321-9876',
+  menu: [
+    {
+      id: 1,
+      name: 'Quantum Quinoa Mushroom Burger',
+      price: 13.0,
+      rating: 4,
+      category: 'mains',
+      details:
+        'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+    },
+    {
+      id: 2,
+      name: 'Binary Berry Cheesecake',
+      price: 10.11,
+      rating: 3,
+      category: 'desserts',
+      details:
+        'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+    },
+    {
+      id: 3,
+      name: 'Recursive Rigatoni',
+      price: 17.0,
+      rating: 5,
+      category: 'mains',
+      details:
+        "A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You'll keep coming back for more."
+    },
+    {
+      id: 4,
+      name: 'Pumpkin Pi Squared',
+      price: 3.14,
+      rating: 5,
+      category: 'desserts',
+      details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+    },
+    {
+      id: 5,
+      name: 'Fibonacci String Bean Fries',
+      price: 11.23,
+      rating: 5,
+      category: 'sides',
+      details:
+        'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
+    }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.status(200).render('home.ejs', { RESTAURANT })
+  // res.send('Hello There!')
+})
+
+app.get('/menu', (req, res) => {
+  res.status(200).render('menu.ejs', { RESTAURANT })
+})
+
+app.get('/menu/:category', (req, res) => {
+  const category = req.params.category
+  const menuItems = RESTAURANT.menu.filter((item) => {
+    return item.category === category
+  })
+  const categoryCapital = category.charAt(0).toUpperCase() + category.slice(1)
+  const restaurantName = RESTAURANT.name
+  res
+    .status(200)
+    .render('category.ejs', { menuItems, categoryCapital, restaurantName })
+})
+
+app.listen(port)
